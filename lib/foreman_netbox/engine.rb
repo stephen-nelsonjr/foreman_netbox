f143c3fb3fb896c1cbd4d87c1001ee2915bdabfd
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-11 14:00:59 -0400

    Update engine.rb

--- lib/foreman_netbox/engine.rb
+++ lib/foreman_netbox/engine.rb
@@ -41,19 +41,20 @@ module ForemanNetbox
     		 menu :top_menu, :level42, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
    	     end
              menu :top_menu, :level5, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
+		
+	     sub_menu :top_menu, :template, :caption=> N_('Netbox IPAM2'), :after=> :hosts_menu, :icon => 'fa fa-tachometer' do
+   	     	menu :top_menu, :level1, :caption=>N_('IP Adresses'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
+   	     	menu :top_menu, :level2, :caption=>N_('Prefixes'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
+   	     	menu :top_menu, :level3, :caption=>N_('Prefix/VLAN Roles'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
+             	sub_menu :top_menu, :inner_level, :caption=> N_('Test') do
+     	        	menu :top_menu, :level41, :caption=>N_('Test'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
+    		 	menu :top_menu, :level42, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
+   	     	end
+             	menu :top_menu, :level5, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
+             end
+
         end
 	
-	sub_menu :top_menu, :template, :caption=> N_('Netbox IPAM2'), :after=> :hosts_menu, :icon => 'fa fa-tachometer' do
-   	     menu :top_menu, :level1, :caption=>N_('IP Adresses'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
-   	     menu :top_menu, :level2, :caption=>N_('Prefixes'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
-   	     menu :top_menu, :level3, :caption=>N_('Prefix/VLAN Roles'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
-             sub_menu :top_menu, :inner_level, :caption=> N_('Test') do
-     	         menu :top_menu, :level41, :caption=>N_('Test'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
-    		 menu :top_menu, :level42, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
-   	     end
-             menu :top_menu, :level5, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
-        end
-
         # add dashboard widget
         widget 'foreman_netbox_widget', name: N_('Foreman plugin template widget'), sizex: 4, sizey: 1
 	      
