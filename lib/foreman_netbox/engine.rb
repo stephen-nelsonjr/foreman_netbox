d035dc13c7ca25eccae7d6de71567dfa02c5b994
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-11 13:20:31 -0400

    Update engine.rb

--- lib/foreman_netbox/engine.rb
+++ lib/foreman_netbox/engine.rb
@@ -37,7 +37,7 @@ module ForemanNetbox
    	     menu :top_menu, :level2, :caption=>N_('Prefixes'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
    	     menu :top_menu, :level3, :caption=>N_('Prefix/VLAN Roles'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
              sub_menu :top_menu, :inner_level, :caption=> N_('Inner level') do
-     	         menu :top_menu, :level41, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
+     	         menu :top_menu, :level41, :caption=>N_('Test'), :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
     		 menu :top_menu, :level42, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
    	     end
              menu :top_menu, :level5, :url_hash => {:controller=> :'foreman_netbox/hosts', :action=>:new_action}
