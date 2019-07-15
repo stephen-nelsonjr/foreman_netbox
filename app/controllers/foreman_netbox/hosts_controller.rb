130654da9f753a1ebc2e2e2869d84e432160f551
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-15 08:03:01 -0400

    Update hosts_controller.rb

--- app/controllers/foreman_netbox/hosts_controller.rb
+++ app/controllers/foreman_netbox/hosts_controller.rb
@@ -5,25 +5,7 @@ module ForemanNetbox
     # layout 'foreman_netbox/layouts/new_layout'
 
     def new_action
-      # automatically renders view/foreman_netbox/hosts/new_action
-      @host = Host.new(host_params)
-      @host.managed = true if (params[:host] && params[:host][:managed].nil?)
-      forward_url_options
-#       if @host.save
-#         process_success :success_redirect => index
-#       else
-#         load_vars_for_ajax
-#         offer_to_overwrite_conflicts
-#         process_error
-#       end
-    end
-    
-    def action_2
-      
-    end
-    
-    def index
-      # automatically renders view/foreman_netbox/hosts/new_action
+      @response = NetboxClient.proxy.get_sections
     end
   end
 end
