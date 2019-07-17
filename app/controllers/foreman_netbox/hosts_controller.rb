45ac4ab373a283d380b96b2213f37a99cd45f63a
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-17 13:46:59 -0400

    Update hosts_controller.rb

--- app/controllers/foreman_netbox/hosts_controller.rb
+++ app/controllers/foreman_netbox/hosts_controller.rb
@@ -3,6 +3,14 @@ module ForemanNetbox
   class HostsController < ::HostsController
     # change layout if needed
     # layout 'foreman_netbox/layouts/new_layout'
+    def ipam
+      @response = Netbox.proxy.get_sections
+    end
+
+    def subnets
+      @subnets = Netbox.proxy.get_subnets(params[:section_id])
+      render :json => @subnets.to_json, :status => :ok
+    end
 
     def new_action
 #       @response = Netbox.proxy.get_sections
