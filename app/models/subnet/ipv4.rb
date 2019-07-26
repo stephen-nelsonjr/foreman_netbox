bf5bfcd4ac9ebe9351c5f9e4b71a65093ec342a1
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-26 08:25:47 -0400

    Update ipv4.rb

--- app/models/subnet/ipv4.rb
+++ app/models/subnet/ipv4.rb
@@ -1,11 +1,11 @@
 require 'socket'
 
 module ForemanNetbox
-  module ipv4 
+#   module ipv4 < subnet
     prepend Subnet::Ipv4 
       def super.supported_ipam_modes
         [:netbox_ipam]
       end
     end
-  end
+#   end
 end
